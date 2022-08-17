8e1d5290a265160951b2cbcae46f9c245eb713b0
aditi2605 <99731059+aditi2605@users.noreply.github.com>
2022-08-17 15:39:50 +0000

    made changes and redone few function in style.css and homepage.css

--- frontend/src/Pages/HomePage/index.js
+++ frontend/src/Pages/HomePage/index.js
@@ -143,7 +143,7 @@ const HomePage = () => {
         <div className="heading"> <p id="leaderboardIcon" onClick={leaderBoard}>
           SCOREBOARD 
         </p></div>
-        <div className="headingh1"><h1> The <span className="fire">M</span><span className="fire">A</span><span className="fire">s</span><span className="fire">t</span><span class="fire">e</span><span className="fire">r</span><span className="fire">m</span><span className="fire">i</span><span className="fire">n</span><span className="fire">d</span>  Quiz </h1></div>
+        <div className="headingh1"><h1> The <span className="fire">M</span><span className="fire">a</span><span className="fire">s</span><span className="fire">t</span><span className="fire">e</span><span className="fire">r</span><span className="fire">m</span><span className="fire">i</span><span class="fire">n</span><span className="fire">d</span>  Quiz </h1></div>
       <div id="welcome">
        <div className="form">
         <form role="form" className="form1" >
