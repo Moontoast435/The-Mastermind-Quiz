5325396f9d8210b9a8f4ccb1955cd3612a454576
Joshua Warner <joshuawarner51@yahoo.com>
2022-08-17 11:37:20 +0100

    changes

--- socketio/Game.js
+++ socketio/Game.js
@@ -1,35 +1,9 @@
-<<<<<<< HEAD
-// const {GameManager} = require("js-gamemanager");
-
-// const games = new GameManager();
-
-=======
->>>>>>> e6fcf89dc7245570599093d570b5823e02296c3e
 class Games {
     constructor() {
-
-        this.games = []; 
+        this.games = [];
         this.players = [];
     }
 
-<<<<<<< HEAD
-    addGame (hostID, roomName, difficulty, count, subject) {
-                let game = {
-                    host: hostID,
-                    room: roomName,
-                    difficulty: difficulty,
-                    count: count,
-                    subject: subject,
-                    players: [],
-                    active: false
-                }
-        
-                // let game = new Game(hostID, roomName, difficulty, count, subject);
-                this.games.push(game);
-   
-                this.games.forEach(room => console.log(room))
-                return game;
-=======
     addGame(hostID, roomName, difficulty, count, subject) {
         let game = {
             host: hostID,
@@ -45,22 +19,10 @@ class Games {
 
         this.games.forEach((room) => console.log(room));
         return game;
->>>>>>> e6fcf89dc7245570599093d570b5823e02296c3e
     }
-        
-    addPlayer (username, room, hostID) {
-        let player = {
-<<<<<<< HEAD
-            username: username, 
-            roomName: room, 
-            roomID: hostID,
-            score: 0
-        }
 
-        this.players.push(player);
-        let game = this.games.find( y => y.room == room);
-        try{
-=======
+    addPlayer(username, room, hostID) {
+        let player = {
             username: username,
             roomName: room,
             roomID: hostID,
@@ -70,55 +32,16 @@ class Games {
         this.players.push(player);
         let game = this.games.find((y) => y.room == room);
         try {
->>>>>>> e6fcf89dc7245570599093d570b5823e02296c3e
             game.players.push(player);
-            console.log(game.players)
+            console.log(game.players);
             return player;
-<<<<<<< HEAD
-        }  catch (err) {
-            console.log("add player has : " + err)
-            return { err: err }   
-=======
         } catch (err) {
             console.log("add player has : " + err);
             return { err: err };
->>>>>>> e6fcf89dc7245570599093d570b5823e02296c3e
         }
     }
 
     getPlayersForGame(roomName) {
-<<<<<<< HEAD
-
-        //get all players
-        const game = this.games.filter(game => game.room === roomName);
-        console.log(game.players)
-    }
-
-
-    filterRoom (roomName) {
-        return this.games.room === roomName; 
-    }
-
-    // getPlayerCount = (roomName) => {
-    //     console.log("check player count")
-    //     const game =  this.games.filter(game => game.room === roomName);
-    //     console.log(game)
-    //     console.log(game.getPlayerCount)
-    //     // console.log(game.players.length); 
-    //     return game.players.length; 
-    // }
-
-    //check the room id
-    //get players with room id
-    getPlayerData (roomName) {
-        //find room in games
-        // console.log("player data")
-        // const game =  this.games.filter(game => game.room === roomName);
-        let game = this.games.find( y => y.room == roomName);
-        // const players = this.players.filter(player => player.room === roomName)
-        if(game === undefined ){
-            return "error"
-=======
         const game = this.games.filter((game) => game.room === roomName);
         console.log(game.players);
     }
@@ -132,25 +55,10 @@ class Games {
 
         if (game === undefined) {
             return "error";
->>>>>>> e6fcf89dc7245570599093d570b5823e02296c3e
         }
         return game.players;
     }
 
-<<<<<<< HEAD
-    addScore(room, username, score){
-        //find the game
-        let game = this.games.find( y => y.room == room);
-        //in the room find the player usernmae
-        console.log(game, 'addscore gameroom')
-        console.log(username, 'addscore username')
-        try{
-            let player = game.players.find(p => p.username === username)
-            //and add the score
-            player.score = score; 
-            //return all player scores for the gamae getPlayerData()
-            return game.players
-=======
     addScore(room, username, score) {
         let game = this.games.find((y) => y.room == room);
 
@@ -165,40 +73,9 @@ class Games {
         } catch (err) {
             console.log("add score error: " + err);
             return { err: err };
->>>>>>> e6fcf89dc7245570599093d570b5823e02296c3e
-        }
-        catch (err) {
-            console.log("add score error: " + err)
-            return { err: err }   
         }
     }
-  
-    getGame (roomName){
-        let game = this.games.find( y => y.room == roomName);
-        return game;
 
-    }
-
-<<<<<<< HEAD
-    canRoomBeJoined(roomName){
-            console.log("Looking for room")
-                const game =  this.games.filter(game => {console.log(game.room === roomName); return game.room === roomName});
-                if (game.length > 0){
-                    return game;
-                } else {
-                    return 'ERROR'
-                }
-    }
-    getGameByRoom(roomName) {
-        this.games.forEach(game => console.log(game))
-        const game =  this.games.filter(game => {console.log(game.room === roomName); return game.room === roomName});
-        return game;
-    };
-
-    checkRoomName(room) {
-        let game = this.getGameByRoom(room);
-        if(game.length > 0 ) {
-=======
     getGame(roomName) {
         let game = this.games.find((y) => y.room == roomName);
         return game;
@@ -228,16 +105,25 @@ class Games {
     checkRoomName(room) {
         let game = this.getGameByRoom(room);
         if (game.length > 0) {
->>>>>>> e6fcf89dc7245570599093d570b5823e02296c3e
             return false;
         } else {
             return true;
-        };
-    };
+        }
+    }
 }
 
-<<<<<<< HEAD
-module.exports = {Games};
-=======
 module.exports = { Games };
->>>>>>> e6fcf89dc7245570599093d570b5823e02296c3e
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
--- socketio/server.js
+++ socketio/server.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const app = express();
-const http = require("http");
-const {Server} = require('socket.io');
-const cors = require("cors")
-app.use(cors());
-const server = http.createServer(app);
-
-const {Games} = require('./Game') 
-=======
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -18,28 +7,19 @@ app.use(cors());
 const server = http.createServer(app);
 
 const { Games } = require("./Game");
->>>>>>> e6fcf89dc7245570599093d570b5823e02296c3e
 
 const io = new Server(server, {
-  cors: {
-      origin: "*",
-      methods: ["GET", "POST"]
-  }
-})
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
 
-<<<<<<< HEAD
-//Makes new instance of Games
-=======
->>>>>>> e6fcf89dc7245570599093d570b5823e02296c3e
 const games = new Games();
 
-//Initialises connected users array
 let users = [];
 
-//Connected socket
 io.on("connection", (socket) => {
-<<<<<<< HEAD
-=======
     socket.emit("assign-id", { id: socket.id });
 
     socket.on("join server", (username) => {
@@ -47,115 +27,24 @@ io.on("connection", (socket) => {
             username,
             id: socket.id,
         };
->>>>>>> e6fcf89dc7245570599093d570b5823e02296c3e
 
+        users.push(user);
+        io.emit("new user", users);
+    });
 
-socket.emit('assign-id', { id: socket.id});
+    let participantCount = io.engine.clientsCount;
+    io.emit("users", participantCount);
 
-socket.on("join server", (username) => {
-  //Declares user object
-  const user = {
-    username,
-    id: socket.id
-  }
-  //Pushes connected user to users array and emits the users array
-  users.push(user);
-  io.emit("new user", users);
-});
-
-
-//Emits number of people online
-  let participantCount = io.engine.clientsCount;
-  io.emit("users", participantCount);
-
-  //Checks if there is room in games array, if not sends back a console.log success message to say room is created (but room isn't actually created until difficulty is chosen)
     socket.on("check-room", (roomName, callback) => {
-<<<<<<< HEAD
-      console.log("CLIENT REQUEST TO CREATE ROOM WITH " ,  roomName)
-      if (games.checkRoomName(roomName)) {
-          callback({code: "success",
-                  message: `SUCCESS: Created room with name ${roomName}`
-              }); 
-      } else {
-          callback({code: "ERROR",
-                    message: `Room name ${roomName} is taken. Please try another name.`
-                  })
-      }
-  });
-
-  //This actually creates the room
-  socket.on('add-config', (config, cb) => {
-
-    games.addGame(config.host, config.room, config.difficulty, config.count, config.subject );
-    socket.join(config.host)
-
-    games.addPlayer(config.username, config.room, config.host)
-
-    cb({
-        code: "success",
-        message: `SUCCESS: configuration has been added`
-    }); 
-})
-socket.on('game-start', (roomName) => {
-  console.log("game started")
-    io.to(roomName).emit('game-start', true)
-});
-
-socket.on('join-room', (config, cb) => {
-  //check room
-  console.log(config);
-  let foundRoom = games.canRoomBeJoined(config.room);
-  console.log('join-room', foundRoom)
-
-  if(foundRoom == 'ERROR') {
-    console.log('no room found')
-    const errorMsg = 'Room does not exist'
-    io.to(config.id).emit("no room", errorMsg)
-  } else {
-          console.log("adding player")
-          games.addPlayer(config.username, config.room, socket.id);
-          socket.join(config.room);
-          socket.emit(`${config.username} has joined the room`);
-          io.emit('new peon', config.username)
-          let game = games.getGameByRoom(config.room);
-
-          cb({
-              code: "success",
-              player: config.username, 
-              score: 0 
-          })
-
-          io.to(game.host).emit("player-connected", { 
-              name: config.username, 
-              score: 0 
-          });
-=======
         console.log("CLIENT REQUEST TO CREATE ROOM WITH ", roomName);
         if (games.checkRoomName(roomName)) {
             callback({ code: "success", message: `SUCCESS: Created room with name ${roomName}` });
             console.log(`Room Created ${roomName}`);
         } else {
             callback({ code: "ERROR", message: `Room name ${roomName} is taken. Please try another name.` });
->>>>>>> e6fcf89dc7245570599093d570b5823e02296c3e
         }
-  }
-)
+    });
 
-<<<<<<< HEAD
-//messages - chatroom
-socket.on("message sent", ({ nicknameChosen, message, room }) => {
-  socket.broadcast.emit("receive message", nicknameChosen, message);
-  console.log('message received:', 'nickname:',nicknameChosen, 'message:',message, 'room:',room)
-});
-
-let gamePlayers; 
-let roomNameVar; 
-socket.on('game-players', (roomName, cb) => {
-    const data = games.getPlayerData(roomName)
-    gamePlayers = data
-    roomNameVar = roomName
-    io.in(roomName).emit(data);
-=======
     socket.on("add-config", (config, cb) => {
         games.addGame(config.host, config.room, config.difficulty, config.count, config.subject);
         socket.join(config.host);
@@ -213,54 +102,22 @@ socket.on('game-players', (roomName, cb) => {
         gamePlayers = data;
         roomNameVar = roomName;
         io.in(roomName).emit(data);
->>>>>>> e6fcf89dc7245570599093d570b5823e02296c3e
 
         cb(data);
     });
 
+    io.to(roomNameVar).emit("game-players");
 
-io.to(roomNameVar).emit('game-players');
+    socket.on("game-start", (roomName) => {
+        console.log("game started");
+        io.to(roomName).emit("game-start", true);
+    });
 
-<<<<<<< HEAD
-    socket.on('game-start', (roomName) => {
-        console.log("game started")
-        io.to(roomName).emit('game-start', true)
-=======
     socket.on("get-questions", (roomName, cb) => {
         const data = games.getGame(roomName);
         cb(data);
->>>>>>> e6fcf89dc7245570599093d570b5823e02296c3e
     });
-  
-    socket.on('get-questions', (roomName, cb) => {
-      const data = games.getGame(roomName)
-      cb(data)
-    })
 
-<<<<<<< HEAD
-
-//Sends scores
-  socket.on('score',  (config, cb) => {
-  console.log("SCORE TALLY")
-  //get data
-  let scores =  games.addScore(config.room, config.username, config.score);
-  io.to(config.room).emit('score', scores);
-  cb({
-      code: "success",
-      scores: scores
-  })
-  });
-
-//On disconnect, count new number of clients and update participantCount
-  socket.on('disconnect', () => {
-    users = users.filter(u => u.id !== socket.id);
-    io.emit("new user", users);
-    //makes io count the number of clients again
-    participantCount = io.engine.clientsCount;
-    io.emit("users", participantCount);
-})
-
-=======
     socket.on("score", (config, cb) => {
         console.log("SCORE TALLY");
         //get data
@@ -279,7 +136,6 @@ io.to(roomNameVar).emit('game-players');
         participantCount = io.engine.clientsCount;
         io.emit("users", participantCount);
     });
->>>>>>> e6fcf89dc7245570599093d570b5823e02296c3e
 });
 
 module.exports = server;
