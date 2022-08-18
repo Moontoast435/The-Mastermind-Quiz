d42f53c9ec55e792229e43a7eedcfa21f15d4616
Moontoast435 <seanmalone1993@gmail.com>
2022-08-18 14:38:15 +0100

    added scoreboard functionality

--- frontend/src/components/QuizResult/index.js
+++ frontend/src/components/QuizResult/index.js
@@ -1,15 +1,54 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect } from 'react';
 import "./QuizResult.css";
+import axios from "axios";
 import Image from "../../assets/MM-LOGO.png";
 
 const QuizResult = ({ result }) => {
-  const navigator = useNavigate();
-
-  const handleRestartOptions = (e) => {
-    e.preventDefault();
-    navigator("../");
+  const navigate = useNavigate();
+  const username = useSelector((state) => state.user.user.username);
+  const handleRestartOptions = () => {
+    
+    navigate("../");
   };
+
+  const score = result.correct;
+  const sendResults = () => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const options = {
+          headers: { "Content-Type": "application/json" },
+        };
+        const results = {
+          player: username,
+          score: score,
+        };
+
+        const { data } = await axios.post(
+          `https://obscure-brushlands-72357.herokuapp.com/scoreBoard`,
+          results,
+          options
+        );
+
+        if (data.err) {
+          throw Error(data.err);
+        }
+        resolve("Scores sent to leaderboard!");
+      } catch (err) {
+        reject(`Can't send results: ${err}`);
+      }
+    });
+  };
+    
+    useEffect(() => {
+      if (score > 0) {
+        sendResults();
+        console.log("sent results");
+      } 
+    }, []);
+  
   return (
     <div className="result-screen-container">
       <img src={Image} alt="background" className="results-page-img"></img>
@@ -26,7 +65,7 @@ const QuizResult = ({ result }) => {
           </span>
           questions.
         </p>
-        <button className="retry-btn" onClick={handleRestartOptions}>
+        <button className="retry-btn" onClick={handleRestartOptions()}>
           Play Again
         </button>
       </div>
