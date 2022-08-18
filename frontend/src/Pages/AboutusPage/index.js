4659e03509a0116e15066e0d8dd40eccdec283a0
aditi2605 <99731059+aditi2605@users.noreply.github.com>
2022-08-18 13:38:47 +0000

    added div to about page

--- frontend/src/Pages/AboutusPage/index.js
+++ frontend/src/Pages/AboutusPage/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './styles.css';
-import image from '../../images/img2.jpg';
-import image2 from '../../images/scroll-png.png';
+// import image from '../../images/img2.jpg';
+// import image2 from '../../images/scroll-png.png';
 
 function AboutusPage() {
   return (
-    <div>
-      <img className="bgImg" src={image} />
-      <img className="scrollPage" src={image2} />
-      <h2>How To Play</h2>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+    <>
+    <div className='aboutUs'>
+      {/*<img className="skullImg" src={image} />*/}
     </div>
+    <div className='aboutUs2'>
+      {/*<img className="scrollPage" src={image2} />*/}
+      <h2 className='h2Tag'>How To Play</h2>
+      <p className='aboutPara'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+    </div>
+    </>
   )
 }
 
