541162daf84ffede9f7a295bfe0f900d23f11cbd
Moontoast435 <seanmalone1993@gmail.com>
2022-08-20 17:15:27 +0100

    change server.get to app.get

--- server/server.js
+++ server/server.js
@@ -11,11 +11,11 @@ const server = http.createServer(app);
 const { Games } = require("./Game");
 
 // Root route
-server.get('/', (req, res) => res.send("Hello, world! Welcome to the masterminds\' API + SOCKETIO Server! "));
+app.get('/', (req, res) => res.send("Hello, world! Welcome to the masterminds\' API + SOCKETIO Server! "));
 
 
 //Leaderboard routes
-server.use('/scoreboard', scoreRouter)
+app.use('/scoreboard', scoreRouter)
 
 const io = new Server(server, {
     cors: {
