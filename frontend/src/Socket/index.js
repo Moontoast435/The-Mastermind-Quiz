fc432b03e364841a224af0648baf8d4599ed6fe5
Moontoast435 <seanmalone1993@gmail.com>
2022-08-20 17:19:31 +0100

    update frontend with new heroku server link

--- frontend/src/Socket/index.js
+++ frontend/src/Socket/index.js
@@ -1,6 +1,6 @@
 
 import io from "socket.io-client";
-const serverEndpoint = "http://localhost:5001"; //add heroku link here
+const serverEndpoint = "https://obscure-brushlands-72357.herokuapp.com/"; //add heroku link here
 const socket = io(serverEndpoint);
 
 export { socket };
